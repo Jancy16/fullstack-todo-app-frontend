61ecde6a9dbe34b87cd4938a0f6ef4ffc92561f5
Jancy M <jancyjancyuma@gmail.com>
2025-02-22 13:20:50 +0530

    Fixed react-scripts issue

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import ToDo from "./components/todo";
+import { addtodo, getAlltodo,updatetodo,deletetodo } from "./utils/HandleApi";
 
 function App() {
+  const [todo,SetToDo] = useState([])
+  const[text,setText] = useState("")
+  const[isUpdating,setisUpdating] = useState(false)
+  const[todoId,settodoId]= useState("")
+  useEffect(()=>{
+    getAlltodo(SetToDo)},[])
+    const updateMode = (_id ,text)=>{
+      setisUpdating(true)
+      setText(text)
+      settodoId(_id)
+    }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container">
+        <h1>ToDo App</h1>
+        <div className="top">
+          <input type="text" placeholder="Add ToDo.."
+          value={text}
+          onChange={(e)=>setText(e.target.value)}/>
+          <div className="add" 
+          onClick={isUpdating ? 
+          ()=> updatetodo(todoId,text,setText,SetToDo,setisUpdating)
+          :()=>addtodo(text,setText,SetToDo)}>
+            {isUpdating ? "Update": "Add"}</div>
+        </div>
+        <div className="list">
+          {todo.map((item)=><ToDo 
+          key={item._id}
+          text={item.text}
+          updateMode={()=>updateMode(item._id,item.text)}
+          deletetodo={()=>deletetodo(item._id,SetToDo)}/>)}
+        </div>
+      </div>
+      </div>
+
+    
   );
 }
 
